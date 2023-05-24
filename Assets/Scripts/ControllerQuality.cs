71fc461160a6091f21eda63df4dd14a94038f262
VladNaboka <naboka.wlad@mail.ru>
2023-05-25 02:16:14 +0600

    add pauseMenu

---
+++ Assets/Scripts/ControllerQuality.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerQuality : MonoBehaviour
+{
+    public GameObject pauseMenu;
+    public void SetQualityLevel(int level)
+    {
+        // Проверить, что уровень находится в допустимом диапазоне
+        if (level < 0 || level > QualitySettings.names.Length - 1)
+        {
+            Debug.LogError("Invalid quality level!");
+            return;
+        }
+
+        // Установить уровень качества графики
+        QualitySettings.SetQualityLevel(level, true);
+    }
+    
+    public void OpenPause()
+    {
+        pauseMenu.SetActive(!pauseMenu.activeSelf);
+        if (pauseMenu.activeSelf)
+            Time.timeScale = 0f;
+        if (!pauseMenu.activeSelf)
+            Time.timeScale = 1f;
+    }
+}
