b8dd4b2334ff8219e25ef5229aaef0f224c08ad7
Isaac-Clark <120954396+Isaac-Clark@users.noreply.github.com>
2023-05-29 23:02:07 +0600

    add speed animation

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -14,7 +14,7 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private CharacterController _characterController;
     [SerializeField] private Animator _animator;
     [SerializeField] private float _switchDelay;
-    private float _playerSpeed = 8f;
+    [SerializeField] private float _playerSpeed = 8f;
     private float _maxPlayerSpeed = 20f;
     private float _controllerHeight = 1.38f;
     private int _lineToMove = 1;
