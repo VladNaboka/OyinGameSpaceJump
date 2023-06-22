7e04135fbb044986c0ee0dcfc4797496cbbdd5d0
VladNaboka <naboka.wlad@mail.ru>
2023-06-22 22:02:33 +0600

    add maxRecord in menu

---
+++ Assets/Scripts/MenuCoinsAndMaxRec.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class MenuCoinsAndMaxRec : MonoBehaviour
+{
+    private GameData _gameData = new GameData();
+    [SerializeField] private TextMeshProUGUI _scoreManagerText;
+    [SerializeField] private TextMeshProUGUI _coinManagerText;
+    private void Awake()
+    {
+        Load();
+    }
+    private void Load()
+    {
+        _gameData = SaveSystem.LoadData();
+        _scoreManagerText.text = _gameData.highscore.ToString();
+        _coinManagerText.text = _gameData.coins.ToString();
+        Debug.Log(_gameData.highscore.ToString());
+    }
+}
