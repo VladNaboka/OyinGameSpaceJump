46826aee308c5aae53348a55ab883a96becc7f1a
VladNaboka <naboka.wlad@mail.ru>
2023-06-25 13:05:22 +0600

    Changing the language

--- Assets/Scripts/SwitchLanguage.cs
+++ Assets/Scripts/SwitchLanguage.cs
@@ -4,44 +4,43 @@ using UnityEngine;
 
 public class SwitchLanguage : MonoBehaviour
 {
-    public GameObject[] slides;
-    private int index = 0;
-    private int countSlides;
-    void Start()
-    {
-        countSlides = slides.Length - 1;
-    }
+    public string[] languages;
+    private int currentLanguageIndex;
 
-    void Update()
+    private void Start()
     {
-        index = Mathf.Clamp(index, 0, countSlides);
-        if (index == 0)
-        {
-            slides[0].gameObject.SetActive(true);
-        }
-
+        currentLanguageIndex = 0;
     }
 
     public void Next()
     {
-        index += 1;
-        index = Mathf.Clamp(index, 0, countSlides);
-
-        for (int i = 0; i < slides.Length; i++)
-        {
-            slides[i].gameObject.SetActive(false);
-            slides[index].gameObject.SetActive(true);
-        }
+        SwitchLanguageController(1);
     }
-    public void Previous()
+    public void Back()
     {
-        index -= 1;
-        index = Mathf.Clamp(index, 0, countSlides);
+        SwitchLanguageController(-1);
+    }
 
-        for (int i = 0; i < slides.Length; i++)
+    private void SwitchLanguageController(int direction)
+    {
+        currentLanguageIndex += direction;
+
+        if (currentLanguageIndex < 0)
         {
-            slides[i].gameObject.SetActive(false);
-            slides[index].gameObject.SetActive(true);
+            currentLanguageIndex = languages.Length - 1;
         }
+        else if (currentLanguageIndex >= languages.Length)
+        {
+            currentLanguageIndex = 0;
+        }
+
+        string selectedLanguage = languages[currentLanguageIndex];
+
+        SwitchToLanguage(selectedLanguage);
+    }
+
+    private void SwitchToLanguage(string language)
+    {
+        PlayerPrefs.SetString("Language", language);
     }
 }
