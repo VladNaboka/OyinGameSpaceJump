f7ebdd7dbaa134641e0da60631609c4d446b0075
4Foshen <dk4ction@mail.ru>
2023-06-27 15:15:27 +0600

    Add text for shop

---
+++ Assets/Scripts/ShopClosed.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShopClosed : MonoBehaviour
+{
+    [SerializeField] private GameObject _textBlock;
+    [SerializeField] private TextMeshProUGUI _textField;
+    [SerializeField] private string[] _texts;
+
+    public void OpenShopTab()
+    {
+        _textBlock.SetActive(false);
+        int randNum = Random.Range(0, _texts.Length + 1);
+
+        _textField.text = _texts[randNum];
+        _textBlock.SetActive(true);
+    }
+}
