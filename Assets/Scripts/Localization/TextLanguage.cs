207e6a8f18c7c68d2bf6b5f3dafa2d6d6698980d
Isaac-Clark <120954396+Isaac-Clark@users.noreply.github.com>
2023-07-04 18:27:01 +0600

    add fonts

--- Assets/Scripts/Localization/TextLanguage.cs
+++ Assets/Scripts/Localization/TextLanguage.cs
@@ -2,12 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using TMPro;
 
 public class TextLanguage : MonoBehaviour
 {
     public string language;
-    TextMeshProUGUI text;
+    Text text;
 
     public string textRu;
     public string textEng;
@@ -15,7 +14,7 @@ public class TextLanguage : MonoBehaviour
 
     void Start()
     {
-        text = GetComponent<TextMeshProUGUI>();
+        text = GetComponent<Text>();
     }
 
     // Update is called once per frame
